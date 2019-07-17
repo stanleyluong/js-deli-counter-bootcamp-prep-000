86c4ba4a4fe3a25ce5bb42d95779f93138e1e5b9
Stanley Luong <slluong@uw.edu>
2019-07-17 00:21:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var katzDeliLine = [];
-
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push((name));
-  return("Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line.");
+var counter = 0;
+function takeANumber(katzDeliLine) {
+  counter += 1 
+  katzDeliLine.push(counter)
+  return("Welcome. " + " You are number " + (counter) + "." );
 }
 
 function currentLine(katzDeliLine, name){
   if (katzDeliLine.length >= 1) {
     let result = "The line is currently: ";
     for (var i = 0; i < katzDeliLine.length; i++) {
-      result += i+1 + ". " + katzDeliLine[i] + " ";
+      result += i+1 + ". " + katzDeliLine[i] + ", ";
     }
-      return(result.slice(0, -1))
+      return(result.slice(0, -2))
 } else {
     return("The line is currently empty.");
   }
@@ -19,9 +20,9 @@ function currentLine(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length >= 1) {
-    return("Currently serving: " +  katzDeliLine.shift());
+    return("Currently serving " +  katzDeliLine.shift() + ".");
      
   } else {
-    return("The line is currently empty.")
+    return("There is nobody waiting to be served!")
   }
 }
